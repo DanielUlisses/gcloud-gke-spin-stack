d302528a4e79c6d9091e6eabcd18d019f2fb09b1
DanielUlisses <danielulisses@outlook.com>
2022-02-17 13:50:52 -0300

    Update accounts for gke

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -1,11 +1,11 @@
-# spinnaker service account and credentials
-resource "google_service_account" "spinnaker_service_account" {
-  account_id   = "spinnaker"
-  display_name = "Spinnaker Service Account"
+# gke service account and credentials
+resource "google_service_account" "gke_service_account" {
+  account_id   = "gke"
+  display_name = "gke Service Account"
 }
 
-resource "google_service_account_key" "spinnaker_service_account" {
-  service_account_id = google_service_account.spinnaker_service_account.name
+resource "google_service_account_key" "gke_service_account" {
+  service_account_id = google_service_account.gke_service_account.name
 }
 
 
@@ -15,7 +15,7 @@ resource "google_project_iam_member" "kubernetes_engine_admin" {
   project = var.project_id
 
   role   = "roles/container.admin"
-  member = "serviceAccount:${google_service_account.spinnaker_service_account.email}"
+  member = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
 
 # access to Container Registry project
@@ -23,14 +23,14 @@ resource "google_project_iam_member" "kubernetes_engine_admin" {
 resource "google_project_iam_member" "container_registry_object_viewer" {
   project = var.project_id
   role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:${google_service_account.spinnaker_service_account.email}"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
 
 
 resource "google_project_iam_member" "docker_registry_project_browser" {
   project = var.project_id
   role    = "roles/browser"
-  member  = "serviceAccount:${google_service_account.spinnaker_service_account.email}"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
 
 # access to GCS artifacts
@@ -38,12 +38,12 @@ resource "google_project_iam_member" "docker_registry_project_browser" {
 resource "google_project_iam_member" "gcs_artifacts_storage_admin" {
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.spinnaker_service_account.email}"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
 
 # run cloudbuild builds
 resource "google_project_iam_member" "cloudbuild" {
   project = var.project_id
   role    = "roles/cloudbuild.builds.builder"
-  member  = "serviceAccount:${google_service_account.spinnaker_service_account.email}"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
 }
